95481a499800e4ba0386245730884803a964f0ed
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-07-03 20:07:18 +0500

    used local storage to avoid api calls for user's ip

--- main.js
+++ main.js
@@ -10,27 +10,48 @@ const submit = document.querySelector('.input');
 window.addEventListener('load', function () {
   const input = document.querySelector('#input');
   input.value = '';
+  response();
 })
 
-const response = async function (event) {
-  event.preventDefault();
+const setItem = function (data) {
+  localStorage.setItem("data", JSON.stringify(data))
+}
+
+const getItem = function () {
+  const data = localStorage.getItem("data")
+  return JSON.parse(data);
+}
+
+const updateHTML = function (data) {
+  document.querySelector('#ip').textContent = data.ip;
+  document.querySelector('#location').textContent = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
+  document.querySelector('#timezone').textContent = `UTC ${data.location.timezone}`;
+  document.querySelector('#isp').textContent = data.isp;
+}
+
+const response = async function () {
   const input = document.querySelector('#input').value;
   try {
-    const res = await axios.post('/.netlify/functions/ipify', { input });
-    const data = res.data;
+    let data;
+    if (input === "" && getItem()) data = getItem();
+    else {
+      const res = await axios.post('/.netlify/functions/ipify', { input });
+      data = res.data;
+    }
 
-    document.querySelector('#ip').textContent = data.ip;
-    document.querySelector('#location').textContent = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
-    document.querySelector('#timezone').textContent = `UTC ${data.location.timezone}`;
-    document.querySelector('#isp').textContent = data.isp;
+    if (input === "" && !getItem()) setItem(data);
 
+    updateHTML(data);
     updateMap(data.location.lat, data.location.lng);
   } catch (err) {
     console.error(err, "API response failed");
   }
 }
 
-submit.addEventListener('submit', response)
+submit.addEventListener('submit', function (event) {
+  event.preventDefault();
+  response();
+})
 
 
 const map = leaflet.map('map').setView([51.505, -0.09], 13);
